5f026047b40adf4fa9a0a41b332f8df4308c8970
Rahmat-Afriyadi <rahmatcv725@gmail.com>
2022-11-11 13:55:28 +0700

    fix time

--- components/DetailAcara.js
+++ components/DetailAcara.js
@@ -13,7 +13,7 @@ export default function DetailAcara(){
                 <p className='font-serif mt-2 z-10 text-2xl'>Minggu</p>
                 <p className='font-mono mt-4 z-10 text-3xl font-bold tanggal'>04</p>
                 <p className='font-serif mt-4 z-10 text-3xl font-semibold'>Desember 2022</p>
-                <p className='font-mono z-10 text-base'>PUKUL 11.00 WIB - SELESAI</p>
+                <p className='font-mono z-10 text-base'>PUKUL 11:00 - 17:00 WIB</p>
                 <p className='font-serif mt-4 z-10 text-3xl font-semibold'>Lokasi Acara </p>
                 <p className='text-base text-center leading-4 z-10'>Jl. Warujaya Kp. Sugutamu No.1 Rt. 02/ Rw. 022 Depok 2 Tengah 16411</p>
                 <motion.div className='w-[170px] h-[39px] bg-[#048bc9] mt-5 rounded-lg pl-3 flex items-center text-base text-white cursor-pointer z-10'
